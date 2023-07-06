7c045dce51c864e45d9f3ace94f2b21bc254a690
jisoo jamie <jisoojamie@gmail.com>
2023-07-06 13:10:49 +0900

    style/Seat

---
+++ TEAM/give-my-seat/src/mocks/seats.js
@@ -0,0 +1,24 @@
+const DUMMY_SEATS = [
+  { id: 1, isEmpty: true },
+  { id: 2, isEmpty: false },
+  { id: 3, isEmpty: true },
+  { id: 4, isEmpty: true },
+  { id: 5, isEmpty: false },
+  { id: 6, isEmpty: true },
+  { id: 7, isEmpty: true },
+  { id: 8, isEmpty: false },
+  { id: 9, isEmpty: true },
+  { id: 10, isEmpty: true },
+  { id: 11, isEmpty: false },
+  { id: 12, isEmpty: true },
+  { id: 13, isEmpty: true },
+  { id: 14, isEmpty: false },
+  { id: 15, isEmpty: true },
+  { id: 16, isEmpty: true },
+  { id: 17, isEmpty: false },
+  { id: 18, isEmpty: true },
+  { id: 19, isEmpty: true },
+  { id: 20, isEmpty: true },
+];
+
+export default DUMMY_SEATS;
--- TEAM/give-my-seat/src/pages/Seat.jsx
+++ TEAM/give-my-seat/src/pages/Seat.jsx
@@ -1,5 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import arrowIcon from "../assets/prev_arrow.svg";
+import seats from "../mocks/seats";
+
+const Title = styled.h4`
+  color: #000000;
+  font-weight: 700;
+  font-size: 20px;
+`;
+const Background = styled.div`
+  background: var(--bg-color);
+  height: 181px;
+  border-radius: 0 0 10px 10px;
+`;
+const SeatsBackground = styled.div`
+  background: #f8f8f8;
+  border-radius: 10px;
+  width: 280px;
+  height: 350px;
+`;
+
+const SeatWrapper = styled.div`
+  width: 42px;
+  height: 39px;
+  border-radius: 10px;
+  background: ${({ isEmpty }) =>
+    isEmpty ? "var(--bg-color)" : "var(--primary-color)"};
+`;
+
 const Seat = () => {
-  return <>Seat</>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Background />
+      <img src={arrowIcon} alt="arrow icon" onClick={() => navigate(-1)} />
+      <Title>카페 여기</Title>
+      <SeatsBackground />
+      {seats.map((seat) => (
+        <SeatWrapper key={seat.id} isEmpty={seat.isEmpty} />
+      ))}
+    </>
+  );
 };
 
 export default Seat;
